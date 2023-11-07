603f261959c43bb155879648973be52f575825af
AlexKrup7 <alexkrup7@yandex.ru>
2023-11-08 02:27:29 +0300

    Add enter key to input

--- base.js
+++ base.js
@@ -3,14 +3,24 @@ const inputAnswer = document.getElementById('inputAnswer')
 const exam = document.getElementById('exam')
 const task = document.getElementById('task')
 const counter = document.getElementById('count')
+const modal = document.querySelector('modal');
+const close = document.querySelector('close');
+document.addEventListener ('keyup', event => {
+    if(event.code === 'Enter' ) {
+        const ex = examination(inputAnswer, result)
+    return ex
+    }
+})
 let count = 0
+
+
 function getRandomnumber(min, max) {
     return Math.random() * (max - min) + min;
   }
 
 function getAll(){
-    num1 = Math.trunc(getRandomnumber(1, 100))
-    num2 = Math.trunc(getRandomnumber(1, 100))
+    num1 = Math.trunc(getRandomnumber(1, 500))
+    num2 = Math.trunc(getRandomnumber(1, 500))
     randomActionIndex = Math.floor(Math.random() * 2)
     expression = randomTask(num1,num2, actions[randomActionIndex])
     result = (randomActionIndex === 0) ? num1 + num2 : num1 - num2
@@ -45,13 +55,15 @@ function examination(inputAnswer, trueSolution) {
         console.log(result)
         }
     else {
-        count--
+        alert ('Ваш счет : ' + count)
+        count = 0
         console.log(count)
         getAll()
         printTask(expression)
         printCounter()
         document.getElementById('inputAnswer').value=''
         console.log(result)
+        
     }
 }
 
